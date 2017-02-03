3b4c8258e26237a6ea2b11095287a815341b5147
Sharigar <alex.akulich@gmail.com>
2017-02-03 20:26:53 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+const app = express();
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+   var now = new Date();
+   var log = `${now} ${req.method} ${req.url}`;
+   
+   console.log(log);
+   fs.appendFile('server.log', log + "\n", (err) => {
+      if (err) console.log('Unable to write log!');
+   });
+   next();
+});
+
+app.use((req, res, next) => {
+   res.render('maintain');
+});
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+   res.render('home', {
+      greetingMessage: 'Hello on my website!!!',
+      pageTitle: 'Home page'
+   });
+});
+
+app.get('/about', (req, res) => {
+   res.render('about', {
+      pageTitle: 'About what?'
+   }); 
+});
+
+app.get('/bad', (req, res) => {
+   res.send('TERRRRIBLE!!!!!!!!'); 
+});
+
+app.listen(8080, () => {
+   console.log('Server is running on port 8080.');
+});
